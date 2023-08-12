d6f597b2f5df1828cbdef113aae4c8113d66c295
Shaikateeqahmed <shaikateeqahmed892@gmail.com>
2023-08-12 21:46:04 +0530

    added get request to index.js

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/",(req,res)=>{
+res.send("Home page");
+})
+
 app.post("/",(req,res)=>{
     try {
         res.status(200).send(req.body);
